16ddb05ccbd6a104cf55ab011d548a35fbebd9ed
Steven Thibault <steve@patternsoft.net>
2015-03-02 09:31:15 -0500

    moved DifferenceParser.h to the main folder

---
+++ DifferenceParser.h
@@ -0,0 +1,14 @@
+#ifndef DIFFERENCEPARSER_H
+#define DIFFERENCEPARSER_H
+
+
+class DifferenceParser
+{
+    public:
+        DifferenceParser();
+        virtual ~DifferenceParser();
+    protected:
+    private:
+};
+
+#endif // DIFFERENCEPARSER_H
